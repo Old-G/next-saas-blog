2a35a953f14c05c2d9ae162ee94ad847ccaa7282
Gleb Zavalov <oldg590@gmail.com>
2023-12-29 00:27:55 +0300

    fix

--- lib/actions/blog.ts
+++ lib/actions/blog.ts
@@ -4,6 +4,7 @@ import { createSupabaseServerClient } from '@/lib/supabase'
 import { IBlog } from '@/lib/types'
 import { revalidatePath, unstable_noStore } from 'next/cache'
 import { BlogFormSchemaType } from '../../app/dashboard/blog/schema'
+import { links } from '../constants/links'
 
 const DASHBOARD = '/dashboard/blog'
 
@@ -86,7 +87,7 @@ export async function updateBlogById(blogId: string, data: IBlog) {
 	const supabase = await createSupabaseServerClient()
 	const result = await supabase.from('blog').update(data).eq('id', blogId)
 	revalidatePath(DASHBOARD)
-	revalidatePath('/blog/' + blogId)
+	revalidatePath(links.blog + blogId)
 	return JSON.stringify(result)
 }
 
@@ -106,7 +107,7 @@ export async function updateBlogDetail(
 			.update({ content: data.content })
 			.eq('blog_id', blogId)
 		revalidatePath(DASHBOARD)
-		revalidatePath('/blog/' + blogId)
+		revalidatePath(links.blog + blogId)
 
 		return JSON.stringify(result)
 	}
@@ -116,6 +117,6 @@ export async function deleteBlogById(blogId: string) {
 	const supabase = await createSupabaseServerClient()
 	const result = await supabase.from('blog').delete().eq('id', blogId)
 	revalidatePath(DASHBOARD)
-	revalidatePath('/blog/' + blogId)
+	revalidatePath(links.blog + blogId)
 	return JSON.stringify(result)
 }
