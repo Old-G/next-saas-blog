cea159fbfbe5de2e9ec0663032700f885325ce87
Gleb Zavalov <oldg590@gmail.com>
2024-01-04 00:54:37 +0300

    fix error

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -1,54 +1,50 @@
-import Stripe from "stripe";
-import { headers } from "next/headers";
-import { createSupbaseAdmin } from "@/lib/supabase";
-import { buffer } from "node:stream/consumers";
+import { createSupabaseAdmin } from '@/lib/supabase'
+import { headers } from 'next/headers'
+import { buffer } from 'node:stream/consumers'
+import Stripe from 'stripe'
 
-const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET!;
+const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET!
 
-const stripe = new Stripe(process.env.STRIPE_SK_KEY!);
+const stripe = new Stripe(process.env.STRIPE_SK_KEY!)
 
 export async function POST(req: any) {
-	const rawBody = await buffer(req.body);
+	const rawBody = await buffer(req.body)
 	try {
-		const sig = headers().get("stripe-signature");
-		let event;
+		const sig = headers().get('stripe-signature')
+		let event
 		try {
-			event = stripe.webhooks.constructEvent(
-				rawBody,
-				sig!,
-				endpointSecret
-			);
+			event = stripe.webhooks.constructEvent(rawBody, sig!, endpointSecret)
 		} catch (err: any) {
-			return Response.json({ error: `Webhook Error ${err?.message!} ` });
+			return Response.json({ error: `Webhook Error ${err?.message!} ` })
 		}
 		switch (event.type) {
-			case "customer.subscription.deleted":
-				const deleteSubscription = event.data.object;
+			case 'customer.subscription.deleted':
+				const deleteSubscription = event.data.object
 				await onCacnelSubscription(
-					deleteSubscription.status === "active",
+					deleteSubscription.status === 'active',
 					deleteSubscription.id
-				);
-				break;
-			case "customer.updated":
-				const customer = event.data.object;
+				)
+				break
+			case 'customer.updated':
+				const customer = event.data.object
 				const subscription = await stripe.subscriptions.list({
 					customer: customer.id,
-				});
+				})
 				if (subscription.data.length) {
-					const sub = subscription.data[0];
+					const sub = subscription.data[0]
 					await onSuccessSubscription(
 						sub.id,
 						customer.id,
-						sub.status === "active",
+						sub.status === 'active',
 						customer.email!
-					);
+					)
 				}
 			default:
-				console.log(`Unhandled event type ${event.type}`);
+				console.log(`Unhandled event type ${event.type}`)
 		}
-		return Response.json({});
+		return Response.json({})
 	} catch (e) {
-		return Response.json({ error: `Webhook Error}` });
+		return Response.json({ error: `Webhook Error}` })
 	}
 }
 
@@ -58,39 +54,39 @@ const onSuccessSubscription = async (
 	status: boolean,
 	email: string
 ) => {
-	const supabase = await createSupbaseAdmin();
+	const supabase = await createSupabaseAdmin()
 	const { data } = await supabase
-		.from("users")
+		.from('users')
 		.update({
 			stripe_subscription_id: subscription_id,
 			stripe_customer_id: customer_id,
 			subscription_status: status,
 		})
-		.eq("email", email)
-		.select("id")
-		.single();
+		.eq('email', email)
+		.select('id')
+		.single()
 	await supabase.auth.admin.updateUserById(data?.id!, {
 		user_metadata: { stripe_customer_id: null },
-	});
-};
+	})
+}
 
 const onCacnelSubscription = async (
 	status: boolean,
 	subscription_id: string
 ) => {
-	const supabase = await createSupbaseAdmin();
+	const supabase = await createSupabaseAdmin()
 	const { data, error } = await supabase
-		.from("users")
+		.from('users')
 		.update({
 			stripe_subscription_id: null,
 			stripe_customer_id: null,
 			subscription_status: status,
 		})
-		.eq("stripe_subscription_id", subscription_id)
-		.select("id")
-		.single();
+		.eq('stripe_subscription_id', subscription_id)
+		.select('id')
+		.single()
 
 	await supabase.auth.admin.updateUserById(data?.id!, {
 		user_metadata: { stripe_customer_id: null },
-	});
-};
+	})
+}
